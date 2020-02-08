7e0f9b99f6bc1b20f3ffd8ac80d79dcb8c059ff9
Setu Potnis <setupotnis@gmail.com>
2020-02-08 16:57:03 -0500

    added background and editing style for text

--- uottafit/src/App.js
+++ uottafit/src/App.js
@@ -3,23 +3,48 @@ import React from 'react';
 
  import './css/index.css';
 import styled from "styled-components";
-import {Image} from 'react-bootstrap';
+import {Image, Jumbotron,Grid,Row,Button,Container} from 'react-bootstrap';
 import Background from "../src/photos/person-holding-barbell-841130.jpg";
 
+var logoStyle = {
+    backgroundImage: "url("+ Background + ")",
+    backgroundSize:"cover", 
+    height: "100%", 
+    width: "100%", 
+    position: 'center'
+}
+var sloganStyle={}
 function App() {
   return (
-
+<React.Fragment>
  <div className="App">
-        <div style={{backgroundImage: "url("+ Background + ")", backgroundSize:"cover", height: "100%", width: "100%", position: 'center'}}>
+     <header>
+
+     </header>
+<body>
+<Container fluid={true}>
+
+      
             <div className="logo" style={{fontFamily:"Montserrat", fontWeight:'bold', fontSize: "250px", color: "white"}}>
                 uOttaFit
             </div>
-            <div className='slogan'>
+            <div className='row' style={{fontFamily:"Montserrat", fontWeight:'bold', fontSize: "250px", color: "white"}}>
+                Lift strong lift safe
 
             </div>
-        </div>
+            <div className = 'row'> blada</div>
+            <div className = 'row'> afds</div>
+            <div className = 'row'> daf</div>
+            <div className = 'row'>fasdf </div>
+            <div className = 'row'>adfasdf </div>
+            <div className = 'row'> afasdfa</div>
+        
+        </Container>
+
+        </body>
 </div>
-    
+
+</React.Fragment>
       
   );
 }
