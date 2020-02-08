7c3391a3502552abe6bd84fc5694d3113d2c2234
Setu Potnis <setupotnis@gmail.com>
2020-02-08 16:35:54 -0500

    added logo and background image

--- uottafit/src/App.js
+++ uottafit/src/App.js
@@ -3,14 +3,21 @@ import React from 'react';
 
  import './css/index.css';
 import styled from "styled-components";
-import Background from "./photos/back.jpg";
 import {Image} from 'react-bootstrap';
+import Background from "../src/photos/person-holding-barbell-841130.jpg";
 
 function App() {
   return (
 
  <div className="App">
-     <img style={{backgroundSize:"fill"}}src={require("./photos/person-holding-barbell-841130.jpg")}></img>
+        <div style={{backgroundImage: "url("+ Background + ")", backgroundSize:"cover", height: "100%", width: "100%", position: 'center'}}>
+            <div className="logo" style={{fontFamily:"Montserrat", fontWeight:'bold', fontSize: "250px", color: "white"}}>
+                uOttaFit
+            </div>
+            <div className='slogan'>
+
+            </div>
+        </div>
 </div>
     
       
