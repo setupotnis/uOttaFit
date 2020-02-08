710c7e266b7fe77e68c4c71d3ffb146d0b9684c2
shaggy3232 <sbrem044@uottawa.ca>
2020-02-08 18:13:49 -0500

    added image to app.css

--- uottafit/src/App.js
+++ uottafit/src/App.js
@@ -2,18 +2,19 @@ import React from 'react';
 
 
  import './css/index.css';
+ import'./css/App.css';
 import styled from "styled-components";
 import {Image, Jumbotron,Grid,Row,Button,Container} from 'react-bootstrap';
 import Background from "../src/photos/person-holding-barbell-841130.jpg";
 
-var logoStyle = {
-    backgroundImage: "url("+ Background + ")",
-    backgroundSize:"cover", 
-    height: "100%", 
-    width: "100%", 
-    position: 'center'
-}
-var sloganStyle={}
+// var logoStyle = {
+//     backgroundImage: "url("+ Background + ")",
+//     backgroundSize:"cover", 
+//     height: "100%", 
+//     width: "100%", 
+//     position: 'center'
+// }
+// var sloganStyle={}
 function App() {
   return (
 <React.Fragment>
@@ -21,27 +22,22 @@ function App() {
      <header>
 
      </header>
-<body>
+<div className = "App-body">
 <Container fluid={true}>
 
       
-            <div className="logo" style={{fontFamily:"Montserrat", fontWeight:'bold', fontSize: "250px", color: "white"}}>
+            <div className="row" style={{fontFamily:"Montserrat", fontWeight:'bold', fontSize: "100px", color: "white"}}>
                 uOttaFit
             </div>
-            <div className='row' style={{fontFamily:"Montserrat", fontWeight:'bold', fontSize: "250px", color: "white"}}>
-                Lift strong lift safe
+            <div className='row' style={{fontFamily:"Montserrat", fontWeight:'bold', fontSize: "50px", color: "white"}}>
+                Lift strong lift safe<div></div>
 
             </div>
-            <div className = 'row'> blada</div>
-            <div className = 'row'> afds</div>
-            <div className = 'row'> daf</div>
-            <div className = 'row'>fasdf </div>
-            <div className = 'row'>adfasdf </div>
-            <div className = 'row'> afasdfa</div>
+     
         
         </Container>
 
-        </body>
+        </div>
 </div>
 
 </React.Fragment>
