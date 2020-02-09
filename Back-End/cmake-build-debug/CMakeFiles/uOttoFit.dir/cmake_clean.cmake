dd6564c99bad14c3093964e71e338def7537a499
dpatroucheva <35235238+dpatroucheva@users.noreply.github.com>
2020-02-08 22:59:37 -0500

    Added back end

---
+++ Back-End/cmake-build-debug/CMakeFiles/uOttoFit.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/uOttoFit.dir/main.cpp.o"
+  "uOttoFit"
+  "uOttoFit.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/uOttoFit.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
