6d992142aa0e22c5e7f14634dcfe44cfcf750b05
Setu Potnis <setupotnis@gmail.com>
2020-02-09 00:13:25 -0500

    added full page functionality for background image

--- uottafit/src/App.js
+++ uottafit/src/App.js
@@ -1,11 +1,8 @@
 import React from 'react';
-
-
- import './css/index.css';
- import'./css/App.css';
+import './css/index.css';
+import'./css/App.css';
 import styled from "styled-components";
 import {Image, Jumbotron,Grid,Row,Button,Container} from 'react-bootstrap';
-import Background from "../src/photos/person-holding-barbell-841130.jpg";
 
 var logoStyle={
     fontFamily:"font-family: Playfair 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif",  
@@ -19,19 +16,21 @@ var sloganStyle ={
     color: "white",
 }
 var fillPage ={
-
+    position: "absolute", 
+    top: 0,
+    left: 0,
+    height: "100%",
+    width: "100%",
 }
 function App() {
   return (
 <React.Fragment>
  <div className="App">
     <header className="App-header">
-        <div>uOttaHack3</div>
+        {/* <div>uOttaHack3</div> */}
     </header>
-<body className = "App-body">
+<body className = "App-body" style={fillPage}>
 <Container >
-
-      
             <div className="row" style={logoStyle}>
                 uOttaFit
             </div>
@@ -40,26 +39,22 @@ function App() {
             </div>
             <div className='row'>
                 <div className="col exercise" >
-                <div>
-                    <p>Deadlift</p>
-                </div>
-
+                    <div>
+                        <p>Deadlift</p>
+                    </div>
                 </div>
                 <div className="col exercise">
-                <div>
-                    <p>Benchpress</p></div>
+                    <div>
+                        <p>Benchpress</p></div>
+                    </div>
+                <div className="col exercise">
+                    <div>
+                        <p>Squat</p>
+                    </div>
                 </div>
-                  <div className="col exercise">
-                  <div>
-                    <p>Squat</p></div>
-                  </div>
-                </div>
-                  
-     
-        
+            </div>
         </Container>
-
-        </body>
+    </body>
 </div>
 
 </React.Fragment>
