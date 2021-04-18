e9352c6bfb547593caef04f77cb443a5be3eea50
Setu Potnis <setupotnis@gmail.com>
2021-04-18 16:24:45 -0400

    added error notification

--- uottafit/src/App.js
+++ uottafit/src/App.js
@@ -1,92 +1,101 @@
-import React,{Component} from 'react';
-import './css/index.css';
-import'./css/App.css';
-import {Image, Jumbotron,Grid,Row,Button,Container} from 'react-bootstrap';
-import {BrowserRouter as Router, Route, Switch,Link,Redirect  } from "react-router-dom";
-import noPageFound from "./404.js";
-import { render } from '@testing-library/react';
-import $ from 'jquery';
+import React from "react";
+import "./css/index.css";
+import "./css/App.css";
+import Alert from "@material-ui/lab/Alert";
+import { Button } from "@material-ui/core";
+import { Container } from "react-bootstrap";
+import { useState } from "react";
 
-var logoStyle={
-    fontFamily:"font-family: Playfair 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif",  
-    fontSize: "100px", 
-    color: "white",
-}
-var sloganStyle ={
-    fontFamily:"font-family: Playfair 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif", 
-    fontWeight:'bold', 
-    fontSize: "50px", 
-    color: "white",
-}
-var fillPage ={
-    position: "absolute", 
-    top: 0,
-    left: 0,
-    height: "100%",
-    width: "100%",
-}
-function runPyScript(){
-  var jqXHR = $.ajax({
-      type: "POST",
-      url: "./test.py",
-      async: false,
-      
-  });
-
-  return jqXHR.responseText;
-}
-class App extends Component {
-  render(){
+function AlertDialog(props) {
   return (
-<React.Fragment>
- <div className="App">
-    <header className="App-header">
-        {/* <div>uOttaHack3</div> */}
-    </header>
-<body className = "App-body" style={fillPage}>
-        <Container>
-            <div className="row" style={logoStyle}>
-                uOttaFit
-            </div>
-            <div className='row' style={sloganStyle}>
-                Lift Strong. Lift Safe.
-            </div>
-            <div className='grid-container grid-container--fit'>
-                <div className='row'>
-                    <div className="col exercise" onClick ={runPyScript} >
-                        <div>
-                        <p>Deadlift</p>
-                        </div>
-                    </div>
-                    <div className="col exercise">
-                        <div>
-                            <p>Benchpress</p></div>
-                        </div>
-                    <div className="col exercise">
-                        <div>
-                            <p>Squat</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </Container>
-    </body>
-</div>
-
-
-{/* <Router>
-        <Switch>
-        <Route exact path="/" component = {App}/>
-        <Route exact path="/App" component = {App}/>
-        <Route exact path= "/404" component ={noPageFound}></Route>  
-        <Redirect to ="/404"/>   
-        </Switch>
-      </Router> */}
-
-</React.Fragment>
-
+    <Alert severity="error">
+      Connect a 3-D Camera in order to have access to full functionality
+    </Alert>
   );
-  }
 }
-
-export default App;
+var logoStyle = {
+  fontFamily:
+    "font-family: Playfair 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif",
+  fontSize: "100px",
+  color: "white",
+};
+var sloganStyle = {
+  fontFamily:
+    "font-family: Playfair 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif",
+  fontWeight: "bold",
+  fontSize: "50px",
+  color: "white",
+};
+var fillPage = {
+  position: "absolute",
+  top: 0,
+  left: 0,
+  height: "100%",
+  width: "100%",
+};
+// function runPyScript() {
+//   var jqXHR = $.ajax({
+//     type: "POST",
+//     url: "./test.py",
+//     async: false,
+//   });
+//   return jqXHR.responseText;
+// }
+export default function App() {
+  const [error, setError] = useState(false);
+  const handleClick = () => {
+    if (!error) setError(true);
+  };
+  return (
+    <>
+      <div className="App">
+        <header className="App-header">{/* <div>uOttaHack3</div> */}</header>
+        <body className="App-body" style={fillPage}>
+          {error && <AlertDialog />}
+          <Container>
+            <div className="row" style={logoStyle}>
+              uOttaFit
+            </div>
+            <div className="row" style={sloganStyle}>
+              Lift Strong. Lift Safe.
+            </div>
+            <div className="grid-container grid-container--fit">
+              <div className="row">
+                <div className="col exercise">
+                  <Button
+                    variant="text"
+                    color="white"
+                    size="large"
+                    onClick={handleClick}
+                  >
+                    Deadlift
+                  </Button>
+                </div>
+                <div className="col exercise">
+                  <Button
+                    variant="text"
+                    color="white"
+                    size="large"
+                    onClick={handleClick}
+                  >
+                    Benchpress
+                  </Button>
+                </div>
+                <div className="col exercise">
+                  <Button
+                    variant="text"
+                    color="white"
+                    size="large"
+                    onClick={handleClick}
+                  >
+                    Squat
+                  </Button>
+                </div>
+              </div>
+            </div>
+          </Container>
+        </body>
+      </div>
+    </>
+  );
+}
--- uottafit/src/Router.js
+++ uottafit/src/Router.js
@@ -1,28 +1,28 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 
-import {BrowserRouter as Router, Route, Switch,Link,Redirect  } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Link,
+  Redirect,
+} from "react-router-dom";
 import App from "./App";
 import Nopagefound from "./Pages/404";
-
-
-
 
 class Router extends Component {
   render() {
     return (
       <React.Fragment>
-       
-      <Router>
-        <Switch>
-          <Route exact path="/" component = {noPageFound}/>
-          <Route exact path="/App" component = {App}/>
-          <Route exact path= "/404" component ={noPageFound}/> 
-          <Redirect to ="/404"/>   
-        </Switch>
-      </Router>
-      
+        <Router>
+          <Switch>
+            <Route exact path="/" component={noPageFound} />
+            <Route exact path="/App" component={App} />
+            <Route exact path="/404" component={noPageFound} />
+            <Redirect to="/404" />
+          </Switch>
+        </Router>
       </React.Fragment>
-      
     );
   }
 }
